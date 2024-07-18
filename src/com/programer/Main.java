c3a0f3c0895fa0abace710f5ad6898e7522e9460
JeanBlanco <jeanrinci.br1@gmail.com>
2024-07-18 13:57:17 -0500

    Sección primera parte

---
+++ src/com/programer/Main.java
@@ -0,0 +1,24 @@
+package com.programer;
+
+/**
+ * Ejemplo de como hacer un hola mundo
+ */
+
+public class Main {
+    /**
+     * Es to es un JavaDoc
+     * @param args - Argumentos de entrada de la aplicación
+     * Punto de entrada de la aplicación
+     * Permite ejecutar la aplicación
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Esto es un comentario
+
+        /*
+        Comentario multilinea.
+         */
+
+    }
+}
---
+++ src/com/programer/TiposDatos.java
@@ -0,0 +1,28 @@
+package com.programer;
+
+public class TiposDatos {
+
+    /**
+     * 1.Numericos
+     * 2.Booleanos
+     * 3.Texto
+     */
+
+    //1.Numericos:
+    //1.1 Enteros:
+    byte variable1 = 5;
+    short variable2 = 6;
+    int variable3 = 8;
+    long variable = 4;
+    //1.2. Decimales:
+    float variable4 = 5.5f;
+    double variable5 = 6.66d;
+
+    //2. Booleanos:
+    boolean house = false;
+    boolean house3 = true;
+
+    //3. Texto:
+    char caracter ='a';
+    String cadenaDeTexto = "programer, developer ";
+}
